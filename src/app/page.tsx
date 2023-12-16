6f574f124c051e05d118bfc5cec7228b622d9a8a
ruanm <247987314@qq.com>
2023-12-16 18:30:16 +0800

    fix: add LogOut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+"use client"
+import { signOut } from "next-auth/react"
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter()
+  
   return (
-    <main className="text-white">Bilibili Clone</main>
+    <main className="text-white">
+      Bilibili Clone
+
+      <button onClick={()=> 
+        signOut({ redirect: false }).then(() => {
+          router.push("/auth"); // Redirect to the home page after signing out
+        })
+      } className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition" >
+        LogOut
+      </button>
+
+    </main>
+
   )
 }
--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -1,8 +1,22 @@
+"use client"
+import { signOut } from "next-auth/react"
+import { useRouter } from "next/navigation";
 
 
 const Profiles = () => {
+  const router = useRouter()
   return (
-    <div>profiles page</div>
+    <div>
+      profiles page
+
+      <button onClick={()=> 
+        signOut({ redirect: false }).then(() => {
+          router.push("/auth"); // Redirect to the home page after signing out
+        })
+      } className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition" >
+        LogOut
+      </button>
+    </div>
   )
 }
 
