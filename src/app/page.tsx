32ad1a877b095b0aa3bc9c73e16144d4bc5f9f76
ruanm <247987314@qq.com>
2023-12-17 17:54:44 +0800

    fix: add Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
 "use client"
-import { signOut } from "next-auth/react"
-import { useRouter } from "next/navigation";
-import { useSession } from 'next-auth/react';
+import Navbar from "~/components/Navbar"
 
 export default function Home() {
-  const router = useRouter()
-
-  const { data: session, status } = useSession();
   
   return (
-    <main className="text-white">
-      Bilibili Clone
-
-      <div>
-      ClientComponent {status}{' '}
-      {status === 'authenticated' && session.user?.name}
-      </div>
-
-      <button onClick={()=> 
-        signOut({ redirect: false }).then(() => {
-          router.push("/auth"); // Redirect to the home page after signing out
-        })
-      } className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition" >
-        LogOut
-      </button>
-
-    </main>
-
+    <>
+      <Navbar />
+    </>
   )
 }
---
+++ src/components/AccountMenu.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { signOut } from "next-auth/react"
+import { useRouter } from "next/navigation";
+import React from "react"
+
+
+interface AccountMenuProps {
+  visible?: boolean;
+}
+const AccountMenu: React.FC<AccountMenuProps> = ({
+  visible
+}) => {
+  const router = useRouter()
+
+  if(!visible){
+    return null;
+  }
+
+  return (
+    <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
+      <div className="flex flex-col gap-3">
+        <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
+          <img className="w-8 rounded-md" src="/images/default-blue.png" alt="" />
+          <p className="text-white text-sm group-hover/item:underline">
+            Username
+          </p>
+        </div>
+        <hr className="bg-gray-600 border-0 h-px my-4"/>
+        <div onClick={()=> {
+          signOut({ redirect: false }).then(() => {
+            router.push("/auth"); // Redirect to the home page after signing out
+          })
+        }} className="px-3 text-center text-white text-sm hover:underline">
+          Sign out of netflix
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AccountMenu
