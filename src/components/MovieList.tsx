5a2dd613ac59ceb3e081ecd98826bfb1db27bc68
ruanm <247987314@qq.com>
2023-12-19 13:13:04 +0800

    fix: Add MovieCard UI and cool hover effects

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { isEmpty } from 'lodash'
+import MovieCard from './MovieCard';
 
 export interface MovieListProps {
   data: Record<string, any>[];
@@ -16,9 +17,9 @@ const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
       <div>
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">{title}</p>
         <div className="grid grid-cols-4 gap-2">
-          { data.map((movie) => {
-            return <div key={movie.id}>Movie</div>
-          }) }
+          { data.map((movie) => (
+            <MovieCard key={movie.id} data={movie}/>
+          )) }
         </div>
       </div>
     </div>
